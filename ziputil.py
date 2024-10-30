873749827dc8d63e26c33977972c72a8ef1bf73e
wuqf <wuqingfeng234@163.com>
2024-10-30 15:40:52 +0800

    清理已打包的文件

--- ziputil.py
+++ ziputil.py
@@ -1,6 +1,9 @@
 import os
 import os.path
+import shutil
 from itertools import count
+from pickle import EMPTY_LIST
+from select import select
 from zipfile import ZipFile
 
 
@@ -44,17 +47,27 @@ class ZipUtil:
                         )
                 print('zip_dirs:', n)
 
+    def remove_zipped_dir(self, zipped_dirs):
+        if zipped_dirs != EMPTY_LIST:
+            for item in zipped_dirs:
+                shutil.rmtree(os.path.join(os.getcwd(), folder_name, item))
+        zipped_dirs.clear()
+
 
 if __name__ == '__main__':
+    folder_name = 'everiaclub'
     z = ZipUtil()
-    path_everiaclub = os.path.join(os.getcwd(), 'everiaclub')
+    path_everiaclub = os.path.join(os.getcwd(), folder_name)
     list = os.listdir(path_everiaclub)
     name_count = 0
+    ziped_list = []
     for i in range(len(list)):
         name = str(name_count) + '.zip'
         if os.path.exists(name) and os.path.getsize(name) > 1.5 * 1024 * 1024 * 1024:
+            z.remove_zipped_dir(ziped_list)
             name_count = name_count + 1
             name = str(name_count) + '.zip'
-        z.zip_dir(dir_i=os.path.join(os.getcwd(), 'everiaclub', list[i]), file_o=name)
+        z.zip_dir(dir_i=os.path.join(os.getcwd(), folder_name, list[i]), file_o=name)
+        ziped_list.append(list[i])
 
     print(list)
